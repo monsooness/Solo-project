57ea1e529ae069e4cb17c9ac06cc1c350bfadabf
Monsoon <nepzsoon@gmail.com>
2018-06-03 01:17:48 -0700

    Added more functionality

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# Solo-project
-Hack Reactor last app practice
+# Solo-project -- Snippity
+
+Goal: To create an app which lets you add over used codes as snippet, which can later be pulled up on sublime as you are coding
+
 - [ ] Input form with single text input
 - [ ] Click handler to capture data in text form field
 - [ ] Helper utilities to read and write to local storage
 
 - [ ] Make it handle code (use a textarea instead of text)
 - [ ] Have two fields, one is a short name and the other is the snippet
-- [ ] Going to store the name and code inside localStorage
-- [ ] Display all snippets
-- [ ] Search for a snippet using the name 
+- [ ] Store the name and code inside localStorage
+- [ ] Dropdown to display all existing snippets on your local storage
--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 $(document).ready(function(){
-	$('.setData').on('click',function(){
-		let textFieldValue = $('.textField').val();
-		console.log(textFieldValue)
-		$('.debug').text(textFieldValue);
+  let snippet_name;
+  let snippet_code;
+  let obj = {}
 
-		localStorage.setItem('myFormTextData', textFieldValue);
-	})
+  $('.setData').on('click', function() {
+    snippet_name = $('.textField').val();
+    snippet_code = $('.codeArea').val();
+    obj[snippet_name] = snippet_code
+    console.log(obj)
 
-	$('.getData').on('click', function(){
-		let retrievedData = localStorage.getItem('myFormTextData');
-		$('.debug').text(retrievedData);
-	});
+    localStorage.setItem(snippet_name, snippet_code);
+    $('.all_info').val('');
+  });
 
 
-});
+
+  // $('.dropdown').on('click', function(){
+  //   alert('i was dropped')
+  //   let retrievedData = localStorage.getItem(snippet_name)
+  // })
+
+  var $dropdown = $("select[name='dropdown']");
+
+  $.each(obj, function(val, text){
+    $('#mySelect').append($('<option></option>')).val(val).html(text)
+      })
+
+  //reset function
+  function ClearFields() {
+     document.getElementsByClassName("buttons").value = "";
+}
+
+
+}) //document.ready
+
+
+
+
+
+
+
+ // $('.getData').on('click', function(){
+  //   let retrievedData = localStorage.getItem('myFormTextData');
+  // });
